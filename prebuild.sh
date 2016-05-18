982b4ff1f245e8bec1c6ad098fb31484417a2496
axt_star@hotmail.com <axt_star@hotmail.com>
2016-05-18 11:39:56 +0900

    add test

--- prebuild.sh
+++ prebuild.sh
@@ -1,2 +1,2 @@
 npm install
-npm install -g eslint webpack
+npm install -g eslint webpack mocha
---
+++ test/unit.js
@@ -0,0 +1,17 @@
+import 'babel-polyfill';
+import "http://code.jquery.com/jquery-1.10.2.min.js";
+import "../src/reversi.jsx";
+
+var assert = require('chai').assert;
+describe('Array', () => {
+  describe('#indexOf()',  () => {
+    it('should return -1 when the value is not present',  () => {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+      
+      // init
+      new 
+      assert.equal(1, 1);
+    });
+  });
+});
